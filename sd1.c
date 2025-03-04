e382275648bb23ab76e0123d7ec6a3c41b73e81c
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2025-03-04 06:15:50 +0600

    Create sd1.c

---
+++ sd1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+struct batsman
+ {
+   char name[25];
+   int runs,score,balls,toruns,ones,twos,threes,fours,sixes;
+   int max_six,max_run,max_four;
+   float str;
+ };
+
+struct bowler
+ {
+   char name[25];
+   int runsgv,wicket,overs;
+   int max_w;
+   float econ;
+ };
+
+ int main() {
+
+ };
